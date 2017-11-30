2338f20d09e5198460288857febdeb03fad46b46
Vito Ao <aoyu@autoio.cn>
2017-11-30 15:40:23 +0800

    Unify code style

--- Signal.hpp
+++ Signal.hpp
@@ -4,29 +4,29 @@
 #include <vector>
 
 namespace simpleSignal {
-template <int>
+template<int>
 struct memberFunctionPlaceholder {};
 }
 
 namespace std {
-    template <int N>
+    template<int N>
     struct is_placeholder<simpleSignal::memberFunctionPlaceholder<N>> : std::integral_constant<int, N + 1> {};
 }
 
 namespace simpleSignal {
 
-template <int... Ns>
+template<int... Ns>
 struct int_sequence {};
 
-template <int N, int... Ns>
+template<int N, int... Ns>
 struct make_int_sequence : make_int_sequence<N - 1, N - 1, Ns...> {};
 
-template <int... Ns>
+template<int... Ns>
 struct make_int_sequence<0, Ns...> : int_sequence<Ns...> {};
 
-template <class> class Signal;
+template<class> class Signal;
 
-template <class ResultType, class... Args>
+template<class ResultType, class... Args>
 class Signal<ResultType(Args...)> {
 public:
     using CallbackType = std::function<ResultType(Args...)>;
@@ -38,7 +38,7 @@ public:
         mCallbacks.emplace_back(std::move(func));
     }
 
-    template <class T, class F>
+    template<class T, class F>
     void connect(T *object, const F &memberFunc) {
         mCallbacks.emplace_back(constructMemberFunc(object, memberFunc, make_int_sequence<sizeof...(Args)>{}));
     }
@@ -54,7 +54,7 @@ public:
     }
 
 private:
-    template <class T, class F, int... Ns>
+    template<class T, class F, int... Ns>
     Signal::CallbackType constructMemberFunc(T *object, const F &memberFunc, int_sequence<Ns...>) const {
         return std::bind(memberFunc, object, memberFunctionPlaceholder<Ns>{}...);
     }
